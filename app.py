7a78cc40ef439991bcb734d318e27eae897b96bc
Rafael <rafael.tavares.92@gmail.com>
2023-10-20 20:38:59 -0300

    api frontend/backend implementado

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def buscar():
     return "<p>Função de busca</p>"
+
+@app.route("/products")
+def products():
+    response = jsonify = ([
+        {
+            "title": "Caneca Personalizada de Porcelana ",
+            "amount": 123.45,
+           " installments": {" number": 3, "total": 41.15, "hasfees": True },
+        },
+        {
+            "title": "Caneca de Choop",
+            "amount": 560.45,
+            "installments": { "number": 6, "total": 38.15, "hasfees": False},
+        }
+    ])
+
+    #response.header.add("Access-Control-Allow-Origin","*")
+
+    return response
+
+
+
+if __name__ == "__main__":
+    app.run(host = 'localhost', port = 5000, debug = True)
